78fff1e076d24c74f5aa0b42d4421e14f9723bd9
karthikchandrabattu <karthik.battu@futureelectronics.com>
2025-06-27 13:29:25 -0400

    Committing 4 items from workspace b8641c1c-64d9-4897-aebc-c6fc4d1bf03b

---
+++ SandBox/WH_AuditLog.Warehouse/scrape/Tables/z7183_findchips_2025-05-20_part_4.csv.sql
@@ -0,0 +1,14 @@
+CREATE TABLE [scrape].[z7183_findchips_2025-05-20_part_4.csv] (
+
+	[MPN] varchar(8000) NULL, 
+	[Distributor] varchar(8000) NULL, 
+	[Manufacturer] varchar(8000) NULL, 
+	[DISTI_NUM] varchar(8000) NULL, 
+	[LeadTime] varchar(8000) NULL, 
+	[Stock] varchar(8000) NULL, 
+	[Qty] varchar(8000) NULL, 
+	[Price] varchar(8000) NULL, 
+	[StockFull] varchar(8000) NULL, 
+	[ScrapeDate] varchar(8000) NULL, 
+	[TimeStamp] varchar(8000) NULL
+);
---
+++ SandBox/WH_AuditLog.Warehouse/scrape/Tables/z7183_findchips_2025-06-05_part_1.csv.sql
@@ -0,0 +1,14 @@
+CREATE TABLE [scrape].[z7183_findchips_2025-06-05_part_1.csv] (
+
+	[MPN] varchar(8000) NULL, 
+	[Distributor] varchar(8000) NULL, 
+	[Manufacturer] varchar(8000) NULL, 
+	[DISTI_NUM] varchar(8000) NULL, 
+	[LeadTime] varchar(8000) NULL, 
+	[Stock] varchar(8000) NULL, 
+	[Qty] varchar(8000) NULL, 
+	[Price] varchar(8000) NULL, 
+	[StockFull] varchar(8000) NULL, 
+	[ScrapeDate] varchar(8000) NULL, 
+	[TimeStamp] varchar(8000) NULL
+);
